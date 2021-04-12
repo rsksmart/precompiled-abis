bc3c5706869b0ecdad3641717a7b9072cfc032f2
Pamela Gonzalez Perilli <pamgonzalezperilli@gmail.com>
2021-04-12 03:47:33 -0300

    New bridge methods are included.
    The methods are included in Bridge's ABI:
    - getActiveFederationCreationBlockHeight
    - getBtcBlockchainBestBlockHeader
    - getBtcBlockchainBlockHeaderByHash
    - getBtcBlockchainBlockHeaderByHeight
    - getBtcBlockchainParentBlockHeaderByHash

--- tests/bridge.test.js
+++ tests/bridge.test.js
@@ -8,7 +8,7 @@ describe('Bridge', () => {
     });
 
     it('has all the expected signatures', () => {
-        assert.equal(bridge.abi.length, 60);
+        assert.equal(bridge.abi.length, 66);
     });
 
     it('builds a valid contract', () => {
