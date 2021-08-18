4c1f6772a79e7d899b909b70b567ec48d76a629e
Marcos <marcos@iovlabs.org>
2021-08-18 19:04:11 -0300

    Add typings file

---
+++ types/index.d.ts
@@ -0,0 +1,11 @@
+export const bridge: {
+    abi: any;
+    address: string;
+    build: (client: any) => any;
+};
+
+export const hdWalletUtils: {
+    abi: any;
+    address: string;
+    build: (client: any) => any;
+};
