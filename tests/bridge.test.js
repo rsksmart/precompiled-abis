5ee9a07062cd67a162ab0b962760d40c92c10981
Antonio Pancorbo <48168255+apancorb@users.noreply.github.com>
2024-09-25 08:56:19 +0000

    feat(abis): add getStateForSvpClient Bridge function

--- tests/bridge.test.js
+++ tests/bridge.test.js
@@ -8,7 +8,7 @@ describe('Bridge', () => {
     });
 
     it('has all the expected signatures', () => {
-        assert.equal(bridge.abi.length, 76);
+        assert.equal(bridge.abi.length, 77);
     });
 
     it('builds a valid contract', () => {
