933c3a057d54bd01378205c29c1c0f373a241921
nathanieliov <nathaniel.calderon@iovlabs.org>
2025-02-24 12:00:45 -0400

    Add test checking abi json has the expected number of function but also that function entries has the mandatory fields

--- tests/bridge.test.js
+++ tests/bridge.test.js
@@ -14,11 +14,26 @@ describe('Bridge', () => {
     });
 
     it('should build the bridge contract with Ethers', () => {
-
         const provider = new ethers.JsonRpcProvider(networkUrl);
         const bridgeContract = new ethers.Contract(bridge.address, bridge.abi, provider);
         assert.equal(bridgeContract instanceof ethers.Contract, true);
-
     });
 
+    it('has all the expected functions with the expected properties', () => {
+        const functions = bridge.abi.filter(item => item.type === 'function');
+        assert.equal(functions.length, 68);
+
+        const allowedStateMutabilities = ["pure", "view", "nonpayable", "payable"];
+        functions.forEach(func => {
+            assert(
+                !func.hasOwnProperty('constant'),
+                `Function '${func.name}' should not have a 'constant' property.`
+            );
+            assert.equal(typeof func.stateMutability, 'string');
+            assert(
+                allowedStateMutabilities.includes(func.stateMutability),
+                `Invalid stateMutability: ${func.stateMutability} for function: ${func.name}`
+            );
+        });
+    });
 });
